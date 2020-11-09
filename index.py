bf9b9f8de64ffb4ab961c9cf8396d21a28cbbbb0
SunYu <nok3118@naver.com>
2020-11-10 07:14:32 +0900

    ok

---
+++ index.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+response = urlopen('https://www.daum.net/')
+soup = BeautifulSoup(response, 'html.parser')
+i = 1
+f = open("새파일.txt", 'w', encoding='utf-8')
+for anchor in soup.select("a.link_favorsch"):
+    data = str(i) + "위: " + anchor.get_text() + "\n"
+    i += 1
+    f.write(data)
+f.close()
